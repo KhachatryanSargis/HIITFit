35c1424ec4b67285f4303ac02de9d23ee239db52
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-07-15 19:21:42 +0400

    Added temporary timer, start/done and history buttons.

--- HIITFit/Views/RatingView.swift
+++ HIITFit/Views/RatingView.swift
@@ -31,49 +31,13 @@
 /// THE SOFTWARE.
 
 import SwiftUI
-import AVKit
 
-struct ExerciseView: View {
-    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-    
-    let index: Int
-    
-    let interval: TimeInterval = 30
-    
+struct RatingView: View {
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                HeaderView(exerciseName: exerciseNames[index])
-                    .padding(.bottom)
-                if let url = Bundle.main.url(
-                    forResource: videoNames[index],
-                    withExtension: "mp4"
-                ) {
-                    VideoPlayer(player: AVPlayer(url: url))
-                        .frame(height: geometry.size.height * 0.45)
-                } else {
-                    Text("Couldn't find \(videoNames[index]).mp4")
-                        .foregroundColor(.red)
-                }
-                Text(
-                    Date().addingTimeInterval(interval),
-                    style: .timer
-                )
-                .font(.system(size: 90))
-                Button("Start/Done") {}
-                    .font(.title3)
-                    .padding()
-                Text("Rating")
-                Spacer()
-                Button("History") {}
-                    .font(.title3)
-                    .padding(.bottom)
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ExerciseView(index: 0)
+    RatingView()
 }
