0ff67e49c3701666f95602e80f17dad9ebfb11a6
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-07-28 20:19:45 +0400

    added RaisedButton and updated Assets

---
+++ HIITFit/Views/RaisedButton.swift
@@ -0,0 +1,27 @@
+//
+//  RaisedButton.swift
+//  HIITFit
+//
+//  Created by Sargis Khachatryan on 28.07.24.
+//
+
+import SwiftUI
+
+struct RaisedButton: View {
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Text("Get Started")
+        })
+    }
+}
+
+struct RaisedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            RaisedButton()
+                .padding(20)
+        }
+        .background(Color("background"))
+        .previewLayout(.sizeThatFits)
+    }
+}
