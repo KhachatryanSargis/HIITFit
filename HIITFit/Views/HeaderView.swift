3a95109d2a95ae467ecf35462e55f46d82ace441
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2024-07-30 20:57:37 +0400

    redesigned header view

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -16,16 +16,24 @@ struct HeaderView: View {
         VStack {
             Text(titleText)
                 .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
             HStack {
                 ForEach(0..<Exercise.exercises.count, id: \.self) { index in
-                    let fill = index == selectedTab ? ".fill" : ""
-                    Image(systemName: "\(index + 1).circle\(fill)")
-                        .onTapGesture {
-                            selectedTab = index
-                        }
+                    ZStack {
+                        Circle()
+                            .frame(width: 32, height: 32)
+                            .foregroundColor(.white)
+                            .opacity(index == selectedTab ? 0.5 : 0)
+                        Circle()
+                            .frame(width: 16, height: 16)
+                            .foregroundColor(.white)
+                    }
+                    .onTapGesture {
+                        selectedTab = index
+                    }
                 }
             }
-            .font(.title2)
         }
     }
 }
